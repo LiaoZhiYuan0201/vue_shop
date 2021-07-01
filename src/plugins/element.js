cc92235da645376244bb39895ab71595ddd96890
zhiyuan <18336915720@163.com>
2021-07-01 20:07:46 +0800

    用户功能列表开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -19,6 +19,13 @@ import {
   Card,
   Row,
   Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
 } from 'element-ui'
 
 Vue.use(Button)
@@ -37,5 +44,12 @@ Vue.use(BreadcrumbItem)
 Vue.use(Card)
 Vue.use(Row)
 Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
