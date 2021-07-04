8423605bf828925e251bb7e5b2f430bb3789a18e
zhiyuan <18336915720@163.com>
2021-07-04 16:31:42 +0800

    完成商品分类功能

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+
+import TreeTable from 'vue-table-with-tree-grid'
+
 //配置请求根路径
 axios.defaults.baseURL='https://lianghj.top:8888/api/private/v1/'
 axios.interceptors.request.use(config=>{
@@ -14,6 +17,8 @@ axios.interceptors.request.use(config=>{
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Welcome from '@/components/welcome.vue'
 import Users from '@/components/user/users.vue'
 import Rights from '@/components/power/rights.vue'
 import Roles from '@/components/power/roles.vue'
+import Cate from '@/components/goods/cate.vue'
 
 
 Vue.use(VueRouter)
@@ -21,35 +22,38 @@ const router = new VueRouter({
     {
       path: '/home',
       component: Home,
-      redirect:'/Welcome',
-      children:[
-        {
-          path:'/Welcome',
-          component:Welcome
+      redirect: '/Welcome',
+      children: [{
+          path: '/Welcome',
+          component: Welcome
         },
         {
-          path:'/users',
-          component:Users
+          path: '/users',
+          component: Users
         },
         {
-          path:'/rights',
-          component:Rights
+          path: '/rights',
+          component: Rights
         },
         {
-          path:'/roles',
-          component:Roles
-        }
+          path: '/roles',
+          component: Roles
+        },
+        {
+          path: '/categories',
+          component: Cate
+        },
       ]
     }
   ]
 })
 
-  router.beforeEach((to, from, next) => {
-    if(to.path=='/login') return next();
-    //获取token 
-    let tokenStr =  window.sessionStorage.getItem('token');
-    if(!tokenStr) return next('/login');
-    next();
-  })
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') return next();
+  //获取token 
+  let tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login');
+  next();
+})
 
 export default router
